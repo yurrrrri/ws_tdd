4144751c1f3bf299b4e43f5c2889a5ffdeb2ade9
yurrrrri <yu_ri_son@naver.com>
2023-03-06 15:58:34 +0900

    4단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -25,6 +25,16 @@ public class App {
                 case "종료":
                     System.out.println("프로그램이 종료되었습니다.");
                     return;
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String authorName = sc.nextLine().trim();
+
+                    long id = ++lastWiseSayingId;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
                 default:
                     System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
                     break;
--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -58,4 +58,41 @@ public class AppTests {
         assertThat(rs)
                 .contains("올바르지 않은 명령입니다.");
     }
+    @Test
+    @DisplayName("등록화면에서 명언과 작가를 입력받고, 명언을 생성한다.")
+    public void t5() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("명언 : ")
+                .contains("작가 : ")
+                .contains("1번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("명언이 등록될 때 마다 생성되는 명언의 번호가 1씩 증가한다.")
+    public void t6() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                나의 죽음을 적들에게 알리지 마라.
+                이순신
+                등록
+                왼손은 거들뿐
+                강백호
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.")
+                .contains("3번 명언이 등록되었습니다.")
+                .doesNotContain("4번 명언이 등록되었습니다.");
+    }
+    // 앱 테스트 끝
 }
